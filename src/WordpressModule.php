bebe120b6ecd2272e17bb17ca2b87fe50655cfdf
Peter Dulacka <dulacka@gmail.com>
2020-03-18 13:17:30 +0100

    WordpressModule authenticator should only be registered when needed
    It shouldn't be enabled by default as such authentication might not
    be required or necessary. Whoever needs this authenticator, it can
    be enabled in own module as per README file.

--- src/WordpressModule.php
+++ src/WordpressModule.php
@@ -6,7 +6,6 @@ use Crm\ApiModule\Api\ApiRoutersContainerInterface;
 use Crm\ApiModule\Authorization\BearerTokenAuthorization;
 use Crm\ApiModule\Router\ApiIdentifier;
 use Crm\ApiModule\Router\ApiRoute;
-use Crm\ApplicationModule\Authenticator\AuthenticatorManagerInterface;
 use Crm\ApplicationModule\CrmModule;
 use Crm\ApplicationModule\SeederManager;
 use Crm\WordpressModule\Api\SyncUserHandler;
@@ -14,14 +13,6 @@ use Crm\WordpressModule\Seeders\ConfigsSeeder;
 
 class WordpressModule extends CrmModule
 {
-    public function registerAuthenticators(AuthenticatorManagerInterface $authenticatorManager)
-    {
-        $authenticatorManager->registerAuthenticator(
-            $this->getInstance(\Crm\WordpressModule\Authenticator\WordpressAuthenticator::class),
-            100
-        );
-    }
-
     public function registerSeeders(SeederManager $seederManager)
     {
         $seederManager->addSeeder($this->getInstance(ConfigsSeeder::class));
